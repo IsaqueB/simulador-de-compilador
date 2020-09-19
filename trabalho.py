f15e0b1a572f94af8811fae5a431066158a36134
IsaqueB <67600830+IsaqueB@users.noreply.github.com>
2020-09-19 07:05:54 -0300

    Add files via upload

---
+++ trabalho.py
@@ -0,0 +1,102 @@
+import random
+
+arquivo = open('arquivo.txt','r')
+f = arquivo.read()
+arquivo.close()
+
+def identificador_tokens(texto):
+    lista = []
+    estado = 1
+    st = ''
+    linha = 1
+    atual = 0
+    while atual < len(texto):
+        if estado == 1:
+            if texto[atual] == ' ':
+                atual+=1
+            elif texto[atual] == '\n':
+                linha+=1
+                atual+=1
+            elif texto[atual] == '%':
+                estado = 6
+                atual+=1
+            elif texto[atual].isalpha():
+                estado = 2
+            elif(texto[atual] == ':'):
+                estado = 4
+            elif texto[atual].isnumeric():
+                estado = 3
+            else:
+                estado = 7
+        elif estado == 2:
+            if texto[atual].isalpha() | texto[atual].isnumeric():
+                st = st + texto[atual]
+                atual+=1
+                if(atual == len(texto)):
+                    Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Identificador','Linha':linha})
+                    lista.append(Token)
+                    st=''
+                    estado=1
+                    atual+=1
+            else:
+                Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Identificador','Linha':linha})
+                lista.append(Token)
+                st=''
+                estado=1
+                atual+=1
+        elif estado == 3:
+            if texto[atual].isnumeric():
+                st=st+texto[atual]
+                atual+=1
+            else:
+                Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Número','Linha':linha})
+                lista.append(Token)
+                st = ''
+                estado = 1
+                atual+=1
+                if(atual == len(texto)):
+                    Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Número','Linha':linha})
+                    lista.append(Token)
+                    st=''
+                    estado=1
+                    atual+=1
+        elif estado == 4:
+            try:
+                if texto[atual+1] == '=':
+                    st = texto[atual]
+                    estado = 5
+                    atual+=1
+                else:
+                    st = texto[atual]
+                    Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Sinal de Atribuição','Linha':linha})
+                    lista.append(Token)
+                    st = ''
+                    estado = 1
+                    atual+=1
+            except:
+                print("aqui")
+        elif estado == 5:
+            st = st + texto[atual]
+            Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Sinal Composto de Atribuição','Linha':linha})
+            lista.append(Token)
+            st = ''
+            estado = 1
+            atual+=1
+        elif estado == 6:
+            if texto[atual] == '\n':
+                linha+=1
+                estado=1
+                atual+=1
+            else:
+                atual+=1
+        elif estado == 7:
+            st = texto[atual]
+            Token = dict({'Valor do Atributo':st,'Classe Gramátical':'Outro Caractere','Linha':linha})
+            lista.append(Token)
+            st = ''
+            estado = 1
+            atual+=1
+    for x in lista:
+        print(x)
+
+identificador_tokens(f)
